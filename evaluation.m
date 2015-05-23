86342728e6b6d4d2cff90c931638815fb37d8f2a
amjed <amjed@amjeds-Air.Home>
2015-05-23 22:23:00 +0200

    add statistics part

--- evaluation.m
+++ evaluation.m
@@ -1,27 +1,28 @@
 clear;
 close all;
 clc;
-
+%%
 sl = load('srLoc.txt');
 sl = sl(:,4);
-slSum = sum(sl)
+slSum = sum(sl);
 
 rl = load('rrLoc.txt');
 rl = rl(:,4);
-rlSum=sum(rl)
+rlSum=sum(rl);
 
 slng = load('sLoc-no-gps.txt');
 slng = slng(:,4);
-slngSum = sum(slng)
+slngSum = sum(slng);
 
 rlng = load('rLoc-no-gps.txt');
 rlng = rlng(:,4);
-rlngSum=sum(rlng)
+rlngSum=sum(rlng);
 
 alng = load('ML-area.txt');
 alng = alng(:,4);
-alngSum=sum(alng)
-
+alngSum=sum(alng);
+%%
+%ploting 
 figure('Position',[440 378 560 620/3]);
 bar([slSum slngSum ; rlSum rlngSum; 0 alngSum ; 0 11 ] );
 ylabel('Joule');
@@ -30,7 +31,6 @@ set(gca , 'xticklabel' , labels);
 set(gca , 'fontsize' , 16);
 set(gca , 'xLim', [0.5 4.5])
 
-
 figure('Position',[440 378 560 620/3]);
 bar([length(sl) length(rl) length(alng) 1  ] );
 ylabel('Number of queries');
@@ -38,3 +38,8 @@ labels = {'SL','ODML' , 'AML' , 'OML'};
 set(gca , 'xticklabel' , labels);
 set(gca , 'fontsize' , 16);
 set(gca , 'xLim', [0.5 4.5])
+%%
+% statistics 
+fprintf(['SL: Number of queries is ' num2str( length(sl) ) ', consumed energy is ' num2str(slSum) '. energy consumed without GPS ' num2str(slngSum) '\n']);
+fprintf(['ODMLL: Number of queries is ' num2str( length(rl) ) ', consumed energy is ' num2str(rlSum) '. energy consumed without GPS ' num2str(rlngSum) '\n']);
+fprintf(['AML: Number of queries is ' num2str( length(alng) ) ', consumed energy is ' num2str(alngSum) '\n']);
